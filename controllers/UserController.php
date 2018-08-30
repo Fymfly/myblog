00e5b9be6664c8ba1234d8f2f8095fb3599d73fc
fengyuemin <fengyuemin@czxy.com>
2018-08-30 16:24:38 +0800

    '添加了MVC文件'

---
+++ controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+namespace controllers;
+
+// 引入模型类
+use models\User;
+
+class UserController {
+    public function hello(){
+        // 取数据
+        $user = new User;
+        $name = $user->getName();
+
+        // 加载视图
+        view('user.hello',[
+            'name' => $name
+        ]);
+    }
+}
